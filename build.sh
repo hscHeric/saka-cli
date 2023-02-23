8253abe237c43f18ebd030b5ddff5a4305472aff
Heric da Silva Cruz <hericsilvaho@gmail.com>
2023-02-23 16:22:41 -0300

    removendo código problematico

--- build.sh
+++ build.sh
@@ -2,15 +2,20 @@
 
 # Verifica se o argumento "-c" foi passado
 if [ "$1" = "-c" ]; then
-  # Remove arquivos gerados pelo CMake
-  rm -rf CMakeCache.txt
-  rm -rf CMakeFiles
-  rm -rf cmake_install.cmake
-  rm -rf Makefile
+    # Remove arquivos gerados pelo CMake
+    rm -rf CMakeCache.txt
+    rm -rf CMakeFiles
+    rm -rf cmake_install.cmake
+    rm -rf Makefile
+    if [ -e "./bin/main" ]; then
+        rm ./bin/main
+    fi
+    
+    rm ./lib/*
 else
-  # Executa o comando cmake
-  cmake .
-
-  # Compila o código
-  make
+    # Executa o comando cmake
+    cmake .
+    
+    # Compila o código
+    make
 fi
--- src/systeminfo.c
+++ src/systeminfo.c
@@ -4,58 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-#ifdef _WIN32
-
-#include <windows.h>
-#include <VersionHelpers.h>
-
-#elif __linux__
-
 #include <unistd.h>
 #include <sys/utsname.h>
 
-#endif
-
 char *getOsName()
 {
-#ifdef _WIN32
-
-    char osName[512];
-    if (IsWindows10OrGreater())
-    {
-        sprintf(osName, "Windows 10 %d.%d.%d",
-                (int)LOBYTE(LOWORD(GetVersion())),
-                (int)HIBYTE(LOWORD(GetVersion())),
-                (int)HIWORD(GetVersion()));
-    }
-    else if (IsWindows8Point1OrGreater())
-    {
-        sprintf(osName, "Windows 8.1 %d.%d.%d",
-                (int)LOBYTE(LOWORD(GetVersion())),
-                (int)HIBYTE(LOWORD(GetVersion())),
-                (int)HIWORD(GetVersion()));
-    }
-    else if (IsWindows8OrGreater())
-    {
-        sprintf(osName, "Windows 8 %d.%d.%d",
-                (int)LOBYTE(LOWORD(GetVersion())),
-                (int)HIBYTE(LOWORD(GetVersion())),
-                (int)HIWORD(GetVersion()));
-    }
-    else
-    {
-        sprintf(osName, "Windows %d.%d.%d",
-                (int)LOBYTE(LOWORD(GetVersion())),
-                (int)HIBYTE(LOWORD(GetVersion())),
-                (int)HIWORD(GetVersion()));
-    }
-    char *osNamePointer = (char *)malloc(sizeof(osName));
-    strcpy(osNamePointer, osName);
-    return osNamePointer;
-
-#elif __linux__
-
     struct utsname osInfo;
     uname(&osInfo);
     char *osNamePointer;
@@ -63,6 +16,4 @@ char *getOsName()
     char *returnValue = strdup(osNamePointer);
     free(osNamePointer);
     return returnValue;
-
-#endif
 }
